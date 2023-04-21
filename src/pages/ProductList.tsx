2d5ecdec9f4947a75e441c18bc304978f81560b5
caoboyeehaw <dylanhoangcao@gmail.com>
2023-04-20 22:28:40 -0500

    vercel hook change

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,10 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import useProducts from './hooks/useProducts';
 import { Product } from '../interfaces/ProductInterface';
 import { useOnClickOutside } from 'usehooks-ts';
+import axios from 'axios';
+import useSWR, { mutate } from 'swr';
 
 const ProductList: React.FC = () => {
-  const { products, isLoading, isError, createProduct, updateProduct, deleteProduct } = useProducts();
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const useProductsHook = () => {
+    const { data, error } = useSWR('/api/products', fetcher);
+
+    const isLoading = !data && !error;
+    const isError = error;
+
+    const updateProduct = async (selectedProduct: Product) => {
+      try {
+        const response = await axios.put(`/api/products?ProductID=${selectedProduct.ProductID}`, selectedProduct);
+        const updatedProduct = response.data;
+        mutate('/api/products');
+        return updatedProduct;
+      } catch (error) {
+        console.error('Error updating product:', error);
+        throw error.response.data;
+      }
+    };
+
+    const createProduct = async (newProduct) => {
+      await fetch('/api/products', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newProduct),
+      });
+
+      mutate('/api/products');
+    };
+
+    const deleteProduct = async (productId) => {
+      await fetch(`/api/products?productId=${productId}`, {
+        method: 'DELETE',
+      });
+
+      mutate('/api/products');
+    };
+
+    return {
+      products: data,
+      isLoading,
+      isError,
+      updateProduct,
+      deleteProduct,
+      createProduct,
+    };
+  };
+
+  const { products, isLoading, isError, createProduct, updateProduct, deleteProduct } = useProductsHook();
 
   const [showAddModal, setShowAddModal] = useState(false);
   const [showModal, setShowModal] = useState(false);
